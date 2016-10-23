9a70aa2eefb72ab60a71032a9752cf6252211219
Namoneo <namoneo@icloud.com>
2016-10-23 19:36:12 +0200

    Photo Model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Song.delete_all
+Photo.delete_all
 Artist.delete_all
 
 #Songs
@@ -19,20 +20,28 @@ song6 = Song.create(name: 'Cheap thrills')
 song7 = Song.create(name: 'Stitches')
 song8 = Song.create(name: 'Lush life')
 song9 = Song.create(name: 'Can’t stop the feeling')
-song10 = Song.create(name: 'Dancing on my own')
+
+# Photos
+photo1 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241048/adele_mmkqov.jpg")
+photo2 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241048/alladin_z5cxsk.jpg")
+photo3 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241049/Zayn_dt5lq1.gif")
+photo4 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241048/Lukas_Graham_mv4gaw.jpg")
+photo5 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241048/Mike_Posner_rqhyxj.jpg")
+photo6 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241049/SIA_znclri.jpg")
+photo7 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241049/Shawn_Mendes_moyisx.png")
+photo8 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241049/Zara_Larsson_bwgtxu.png")
+photo9 = Photo.create(remote_image_url: "http://res.cloudinary.com/dxgzovwd1/image/upload/v1477241048/Justin_Timberlake_xmqqap.jpg")
 
 # Artists
-
-artist1 = Artist.create(name: 'Adele')
-artist2 = Artist.create(name:'Aladdin')
-artist3 = Artist.create(name:'Zayn')
-artist4 = Artist.create(name:'Lukas Graham')
-artist5 = Artist.create(name:'Mike Posner')
-artist6 = Artist.create(name:'SIA')
-artist7 = Artist.create(name:'Shawn Mendes')
-artist8 = Artist.create(name:'Zara Larsson')
-artist9 = Artist.create(name:'Justin Timberlake')
-artist10 = Artist.create(name:'Calum Scott')
+artist1 = Artist.create(name: 'Adele', photos: [photo1])
+artist2 = Artist.create(name: 'Aladdin', photos: [photo2])
+artist3 = Artist.create(name:'Zayn', photos: [photo3])
+artist4 = Artist.create(name:'Lukas Graham', photos: [photo4])
+artist5 = Artist.create(name:'Mike Posner', photos: [photo5])
+artist6 = Artist.create(name:'SIA', photos: [photo6])
+artist7 = Artist.create(name:'Shawn Mendes', photos: [photo7])
+artist8 = Artist.create(name:'Zara Larsson', photos: [photo8])
+artist9 = Artist.create(name:'Justin Timberlake', photos: [photo9])
 
 artist1.songs << [song1]
 artist1.save
@@ -60,6 +69,3 @@ artist8.save
 
 artist9.songs << [song9]
 artist9.save
-
-artist10.songs << [song10]
-artist10.save
