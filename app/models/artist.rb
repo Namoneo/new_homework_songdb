e56172ab14a617d8f82f6c0731a6378a1dbd85a8
Namoneo <namoneo@icloud.com>
2016-11-02 16:18:05 +0100

    Saving comments

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,12 +2,12 @@ class Artist < ApplicationRecord
   has_many :songs
   has_many :photos
 
-  def self.order_by_name
-      order(:name)
-  end
-
-  def self.order_created_at
-      order(:created_at)
-  end
+  # def self.order_by_name
+  #     order(:name)
+  # end
+  #
+  # def self.order_created_at
+  #     order(:created_at)
+  # end
 
 end
